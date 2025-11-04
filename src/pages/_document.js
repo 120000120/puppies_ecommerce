27f9dad20e01dbb5c68dc586fc8ae87a24acc64d
120000120 <bestfamilypuppiesgroup@gmail.com>
2025-11-04 17:16:56 -0600

    Create _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        <meta name="facebook-domain-verification" content="7zmabwoyp3l87xlx4" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
